321e814ad667f5c3da0e8f326c82ae2454fcbb43
kuznemai <marinakuznetsova420@gmail.com>
2024-08-04 17:34:09 +0300

    cart, shipping and login pages were added

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', appearOnScroll);
     appearOnScroll(); // Запускаем функцию при загрузке страницы для проверки видимости изначально
 });
+
+
+
+// Button
+
+
+// Добавляем в корзину
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.buy-button');
+    const cart = document.getElementById('cart');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const productName = button.getAttribute('data-product');
+            const productPrice = button.getAttribute('data-price');
+
+            const listItem = document.createElement('li');
+            listItem.textContent = `${productName} - $${productPrice}`;
+            cart.appendChild(listItem);
+        });
+    });
+});
+
+
+
